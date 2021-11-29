0d206243234f855c858721c73dbe29aa1e8bef9e
Thomas Montalto <thomasmonta98@gmail.com>
2021-11-29 21:44:02 +1100

    done with assignment

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,9 @@
 module ArtistsHelper
+  def display_artist(song)
+    if song.artist
+      "<a href=#{artist_path(song.artist)}>#{song.artist_name}</a>".html_safe
+    else
+      "<a href=#{edit_song_path(song)}>Add Artist</a>".html_safe
+    end
+  end
 end
